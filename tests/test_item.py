371b5568948f8308fcbebc114b2a82a98af261a5
Artem Blodov <mr.samy@rambler.ru>
2023-10-14 15:01:47 +0200

    complit DZ

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,19 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+from homework_1.main import main
+
+item1 = Item("Смартфон", 10000, 20)
+item2 = Item("Ноутбук", 20000, 5)
+
+
+def test_calculate_total_prise():
+    assert item1.calculate_total_price() == 200000
+    assert item2.calculate_total_price() == 100000
+    assert main() is None
+
+
+def test_pay_rate():
+    Item.pay_rate = 0.8
+    item1.apply_discount()
+    assert item1.price == 8000

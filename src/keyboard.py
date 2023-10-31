5ab924fd12f00154a7dcadc579d49b6002f98463
Artem Blodov <mr.samy@rambler.ru>
2023-10-31 18:40:12 +0200

    hw5

--- src/keyboard.py
+++ src/keyboard.py
@@ -4,22 +4,22 @@ from src.item import Item
 class LangKeyboard:
     lang = ['EN', 'RU']
 
+    def __init__(self):
+        self.__language = self.lang[0]
+
     def change_lang(self):
-        if self.__language == LangKeyboard.lang[0]:
-            self.__language = LangKeyboard.lang[1]
+        if self.__language == self.lang[0]:
+            self.__language = self.lang[1]
         else:
-            self.__language = LangKeyboard.lang[0]
-
-
-class Keyboard(Item, LangKeyboard):
-
-    def __init__(self, name, price, quantity, language='EN'):
-        super().__init__(name, price, quantity)
-        self.__language = language
-
-    def __str__(self):
-        return self.name
+            self.__language = self.lang[0]
 
     @property
     def language(self):
         return self.__language
+
+
+class Keyboard(Item, LangKeyboard):
+
+    def __init__(self, name, price, quantity):
+        super().__init__(name, price, quantity)
+        LangKeyboard.__init__(self)
